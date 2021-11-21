53dd412e94e58a0aa864b986175c5905e62dd4c8
MOHAMED RIAD <mohamedriiad@gmail.com>
2021-11-21 19:46:02 +0100

    Adding some css properties & icons

--- src/components/NewItemForm.js
+++ src/components/NewItemForm.js
@@ -1,4 +1,5 @@
 import AddCircleIcon from "@mui/icons-material/AddCircle";
+import CancelIcon from "@mui/icons-material/Cancel";
 import { useState } from "react";
 import classes from "./NewItemForm.module.css";
 
@@ -24,8 +25,8 @@ function NewItemForm({ addNewItem, cancelAdding }) {
   const pointerStyle = { cursor: "pointer" };
 
   return (
-    <div>
-      <button onClick={onCancelClickHandler}>Cancel</button>
+    <div className={classes.newItem__control__container}>
+      <CancelIcon onClick={onCancelClickHandler} style={pointerStyle} />
       <input
         className={classes.newItem__control}
         type="text"
