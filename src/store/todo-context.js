7995bcad480bb7727ba1d739e317a2cf428839b9
MOHAMED RIAD <mohamedriiad@gmail.com>
2021-12-15 22:47:37 +0100

    Delete deprecated code

--- src/store/todo-context.js
+++ src/store/todo-context.js
@@ -6,32 +6,6 @@ const TodoContext = React.createContext();
 export const TodoContextProvider = (props) => {
 	const [todoState, dispatchTodo] = useReducer(todoReducer, initialList);
 
-	/*const [isNoData, setIsNoData] = useState(false);
-	const [isAdding, setIsAdding] = useState(false);
-	const [enteredText, setEnteredText] = useState('');
-
-	const onAddClickHandler = (enteredText) => {
-		//setIsAdding(false);
-		setIsNoData(false);
-		setEnteredText(enteredText);
-		dispatchTodo({
-			type: 'ADD_ITEM',
-			payload: { contentText: enteredText, contentId: uid() },
-		});
-	};
-
-	const onBackDropShow = () => {
-		setIsAdding(true);
-	};
-
-	const onBackDropHide = () => {
-		setIsAdding(false);
-	};
-
-	const onNoDataIsAdded = () => {
-		setIsNoData(true);
-	};*/
-
 	return (
 		<TodoContext.Provider value={[todoState, dispatchTodo]}>
 			{props.children}
